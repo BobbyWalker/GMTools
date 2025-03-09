1530dbe5cbd5af561ce1d99d952d1cabd1494df4
Bobby Walker <BobbyWalker@users.noreply.github.com>
2025-03-09 16:37:47 -0500

    Initial Commit

---
+++ GMTools/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GMTools
+//
+//  Created by Bobby Walker on 3/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GMTools/GMToolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GMToolsApp.swift
+//  GMTools
+//
+//  Created by Bobby Walker on 3/9/25.
+//
+
+import SwiftUI
+
+@main
+struct GMToolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
